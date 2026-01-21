988927b435281e721b144ebdff16cbe94f0a7662
kim <kim@gmail.com>
2026-01-21 15:33:39 +0800

    Refactor code structure for improved readability and maintainability

---
+++ ng-services-demo/src/app/products.ts
@@ -0,0 +1,35 @@
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class Products {
+  getProducts() {
+    return [
+      {
+        id: 'P-101',
+        name: 'Logitech Mouse',
+        description: '6 Button Mechanical Mouse',
+        price: '899.00',
+      },
+      {
+        id: 'P-102',
+        name: 'JBL BT Speaker',
+        description: 'Waterproof Radio 360 Surround',
+        price: '1,099.00',
+      },
+      {
+        id: 'P-103',
+        name: 'Mechanical Keyboard',
+        description: 'Hot-swappable RGB Backlit',
+        price: '2,395.00',
+      },
+      {
+        id: 'P-104',
+        name: 'Oculus Meta',
+        description: 'All-in-one Gaming Headset',
+        price: '22,450.00',
+      },
+    ]
+  }
+}
